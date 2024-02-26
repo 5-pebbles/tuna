5390795cf3829801362feb3d7d8d3378c54fdc6b
5-pebbles <5-pebble@protonmail.com>
2024-02-26 18:58:53 +0000

    feat(database): add sqlite migrations for albums, artists & tracks.

--- src/database/database.rs
+++ src/database/database.rs
@@ -35,6 +35,73 @@ CREATE TABLE IF NOT EXISTS users (
                 ",
                 params![],
             )?;
+
+            tx.execute(
+                "
+CREATE TABLE IF NOT EXISTS artists (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    genres TEXT NOT NULL DEFAULT '',
+    bio TEXT NOT NULL DEFAULT ''
+);
+                ",
+                params![],
+            )?;
+
+            tx.execute(
+                "
+CREATE TABLE IF NOT EXISTS albums (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    release INTEGER NOT NULL DEFAULT  0,
+    genres TEXT NOT NULL DEFAULT '',
+    count INTEGER NOT NULL DEFAULT 1
+);
+                ",
+                params![],
+            )?;
+
+            tx.execute(
+                "
+CREATE TABLE album_artist (
+    album_id TEXT NOT NULL,
+    artist_id TEXT NOT NULL,
+    PRIMARY KEY (album_id, artist_id),
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    FOREIGN KEY (artist_id) REFERENCES artists(id)
+);
+                ",
+                params![],
+            )?;
+
+            tx.execute(
+                "
+CREATE TABLE tracks (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    release INTEGER NOT NULL DEFAULT  0,
+    duration INTEGER NOT NULL DEFAULT  0,
+    segments TEXT NOT NULL DEFAULT '',
+    genres TEXT NOT NULL DEFAULT '',
+    lyrics TEXT NOT NULL DEFAULT ''
+);
+                ",
+                params![],
+            )?;
+
+            tx.execute(
+                "
+CREATE TABLE track_album (
+    track_id TEXT NOT NULL,
+    album_id TEXT NOT NULL,
+    PRIMARY KEY (track_id, album_id),
+    FOREIGN KEY (track_id) REFERENCES tracks(id),
+    FOREIGN KEY (album_id) REFERENCES albums(id)
+);
+                ",
+                params![],
+            )?;
+
             tx.commit()?;
             Ok(())
         })
