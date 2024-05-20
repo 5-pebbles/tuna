fa1c49a05fb51d7d7f6f26dd9b416a33039964bb
5-pebbles <5-pebble@protonmail.com>
2024-05-20 19:48:09 -0400

    feat(lib): add lib.rs;

---
+++ tuna-server/src/lib.rs
@@ -0,0 +1,35 @@
+#![allow(clippy::too_many_arguments)]
+
+#[macro_use]
+extern crate rocket;
+
+use std::{fs, path::Path};
+
+mod api;
+mod database;
+mod docs;
+mod error;
+
+fn create_working_dir() {
+    fn ensure_dir(dir: &str) {
+        let path = Path::new(dir);
+        if !path.exists() {
+            fs::create_dir_all(path).expect("Failed to create directory");
+        }
+    }
+    ensure_dir("./database/sqlite");
+    ensure_dir("./database/audio");
+}
+
+pub fn launch() {
+    create_working_dir();
+
+    rocket::execute(
+        rocket::build()
+            .attach(database::fairing())
+            .attach(api::fairing())
+            .attach(docs::fairing())
+            .launch(),
+    )
+    .unwrap();
+}
--- tuna-server/src/main.rs
+++ tuna-server/src/main.rs
@@ -1,32 +1,5 @@
-#![allow(clippy::too_many_arguments)]
+use tuna_server::launch;
 
-#[macro_use]
-extern crate rocket;
-
-use std::{fs, path::Path};
-
-mod api;
-mod database;
-mod docs;
-mod error;
-
-fn create_working_dir() {
-    fn ensure_dir(dir: &str) {
-        let path = Path::new(dir);
-        if !path.exists() {
-            fs::create_dir_all(path).expect("Failed to create directory");
-        }
-    }
-    ensure_dir("./database/sqlite");
-    ensure_dir("./database/audio");
-}
-
-#[launch]
-fn rocket() -> _ {
-    create_working_dir();
-
-    rocket::build()
-        .attach(database::fairing())
-        .attach(api::fairing())
-        .attach(docs::fairing())
+fn main() {
+    launch();
 }
